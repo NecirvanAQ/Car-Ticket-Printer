fb257a3dddf02800e195d2d5fb6942e50a2bf081
Necirvan <AQ231029@aquinas.ac.uk>
2023-09-20 15:53:52 +0100

    Added a printing system v(1.3)
    Printing system added, still needs refining.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Xml;
+using System.Threading;
 
 namespace Program
 {
@@ -10,7 +11,16 @@ namespace Program
         {
 
             string reg = RegGetter();
-            string duration = DurationGetter();
+            string durationString = DurationGetter();
+            double due = Pay(durationString);
+
+            System.TimeSpan duration = new System.TimeSpan(0, Convert.ToInt32(durationString), 0, 0);
+
+            Console.Clear();
+
+            Print(duration, reg, due);
+
+            Console.ReadKey();
 
         }
 
@@ -21,7 +31,7 @@ namespace Program
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.Clear();
             Console.ForegroundColor = ConsoleColor.Black;
-            
+
 
             Console.WriteLine("##################################################################");
             Console.WriteLine("");
@@ -47,7 +57,7 @@ namespace Program
                 reg = Console.ReadLine();
                 Console.WriteLine("Registration entered: " + reg);
                 Console.WriteLine("");
-                
+
                 Console.WriteLine("Is this correct?  (y/n)");
                 correct = Console.ReadLine();
 
@@ -81,12 +91,12 @@ namespace Program
                 Console.WriteLine("Please select duration:          (Press number 1 - 7)");
                 Console.WriteLine("");
                 Console.WriteLine("1       Up to 30 minutes       £1");
-                Console.WriteLine("1       30-60 minutes          £1");
-                Console.WriteLine("1       Up to 2 hours          £1");
-                Console.WriteLine("1       Up to 3 hours          £1");
-                Console.WriteLine("1       Up to 4 hours          £1");
-                Console.WriteLine("1       Up to 6 hours          £1");
-                Console.WriteLine("1       Up to 12 hours         £1");
+                Console.WriteLine("2       30-60 minutes          £1");
+                Console.WriteLine("3       Up to 2 hours          £1");
+                Console.WriteLine("4       Up to 3 hours          £1");
+                Console.WriteLine("5       Up to 4 hours          £1");
+                Console.WriteLine("6       Up to 6 hours          £1");
+                Console.WriteLine("7       Up to 12 hours         £1");
                 Console.WriteLine("");
 
                 reg = Console.ReadLine();
@@ -112,7 +122,7 @@ namespace Program
             return reg;
         }
 
-        static int Pay(string duration)
+        static double Pay(string duration) /* handles payment and expenses change */
         {
             double due = 0;
             double payed = 0;
@@ -162,17 +172,126 @@ namespace Program
                 Console.WriteLine("1       Up to 6 hours          £1");
                 Console.WriteLine("1       Up to 12 hours         £1");
                 Console.WriteLine("");
-                Console.WriteLine("Amount tendered: "+payed);
+                Console.WriteLine("Amount tendered: " + payed);
                 Console.WriteLine("");
 
-                Console.Write("Please enter the coins to the value of" + due);
+                Console.WriteLine("Please enter the coins to the value of £" + due);
+                string lastCoin = "";
 
-                
+                while (true)
+                {
+
+
+                    lastCoin = Console.ReadLine();
+                    try
+                    {
+                        payed += Convert.ToDouble(lastCoin);
+                        break;
+                    }
+                    catch
+                    {
+                        Menu();
+
+                        Console.WriteLine("Please select duration:          (Press number 1 - 7)");
+                        Console.WriteLine("");
+                        Console.WriteLine("1       Up to 30 minutes       £1");
+                        Console.WriteLine("1       30-60 minutes          £1");
+                        Console.WriteLine("1       Up to 2 hours          £1");
+                        Console.WriteLine("1       Up to 3 hours          £1");
+                        Console.WriteLine("1       Up to 4 hours          £1");
+                        Console.WriteLine("1       Up to 6 hours          £1");
+                        Console.WriteLine("1       Up to 12 hours         £1");
+                        Console.WriteLine("");
+                        Console.WriteLine("Amount tendered: " + payed);
+                        Console.WriteLine("");
+
+                        Console.WriteLine("Make sure you enter a number!");
+                        Console.WriteLine("Please enter the coins to the value of £" + due);
+                    }
+                }
 
             }
 
-            return reg;
+            Console.WriteLine("");
+            Console.WriteLine("Amount entered: " + payed);
+            if (payed > due)
+            {
+                Console.WriteLine("Change dispensed: " + (payed - due));
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Please wait...");
+            Thread.Sleep(2000);
+            return due;
         }
 
+        static void Print(TimeSpan duration, string reg, double due)
+        {
+            Console.WriteLine("");
+
+            Console.BackgroundColor = ConsoleColor.White;
+
+            Console.WriteLine("                                 ");
+            Thread.Sleep(100);
+            Console.WriteLine("   B O B ' S   C A R   P A R K   ");
+            Thread.Sleep(100);
+            Console.WriteLine("                                 ");
+            Thread.Sleep(100);
+            Console.WriteLine("                                 ");
+            Thread.Sleep(100);
+            Console.WriteLine("Registration: {0}                ", reg);
+            Thread.Sleep(100);
+            Console.WriteLine("                                 ");
+            Thread.Sleep(100);
+            Console.WriteLine("                                 ");
+            Thread.Sleep(100);
+            Console.WriteLine("Entry: {0}           ", DateTime.Now);
+            Thread.Sleep(100);
+            Console.WriteLine("                                 ");
+            Thread.Sleep(100);
+            Console.WriteLine("                                 ");
+            Thread.Sleep(100);
+            Console.WriteLine("Fee:         £{0}                 ", due);
+            Thread.Sleep(100);
+            Console.WriteLine("                                 ");
+            Thread.Sleep(100);
+            Console.WriteLine("                                 ");
+            Thread.Sleep(100);
+            Console.WriteLine("Expiry: " +((DateTime.Now).Add(duration))+"    ");
+            Thread.Sleep(100);
+            Console.WriteLine("                                 ");
+            Thread.Sleep(100);
+            Console.WriteLine("                                 ");
+            Thread.Sleep(100);
+            Console.WriteLine("Have a nice day!                 ");
+            Thread.Sleep(100);
+            Console.WriteLine("                                 ");
+            Thread.Sleep(100);
+            Console.WriteLine("                                 ");
+            Thread.Sleep(100);
+
+        }
     }
 }
+
+
+
+/*
+
+   B O B ' S   C A R   P A R K
+
+
+Registration: ABC
+
+
+Entry: 20/09/2023 15:37:20
+
+
+Fee:         £1
+
+
+Expiry: 20/09/2023 16:07:20
+
+
+        Have a nice day!
+
+*/
